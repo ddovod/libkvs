4a73e6c3cd7ef3cf61eb27cf53bd0e8a006dc2ed
ddovod <ddovod@gmail.com>
2019-08-26 13:22:10 +0300

    adding simple example application

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,8 +1,108 @@
 
+#include <VirtualStorage.hpp>
+#include <cstdlib>
 #include <iostream>
+#include <memory>
+#include "IStorage.hpp"
+#include "IVirtualStorage.hpp"
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
-    return 0;
+    auto storage = std::make_unique<kvs::VirtualStorage>();
+
+    {  // Volume 1
+        kvs::MountOptions options;
+        options.storageMountPath = "A/B";
+        options.volumeFilePath = "volume1.kvs";
+        options.volumeMountPath = "K/L";
+        auto result = storage->mount(options);
+        if (!result.isOk()) {
+            std::abort();
+        }
+    }
+
+    {  // Volume 2
+        kvs::MountOptions options;
+        options.storageMountPath = "A/B/C";
+        options.volumeFilePath = "volume2.kvs";
+        options.volumeMountPath = "P/C/R";
+        auto result = storage->mount(options);
+        if (!result.isOk()) {
+            std::abort();
+        }
+    }
+
+    for (int i = 0; i < 100; i++) {
+        auto key = "key_" + std::to_string(i);
+        auto value = i;
+        kvs::Status status;
+        switch (i % 2) {
+            case 0: {
+                status = storage->putValue({key, "A/B"}, {value});
+                break;
+            }
+            case 1: {
+                status = storage->putValue({key, "A/B/C"}, {value});
+                break;
+            }
+        }
+
+        if (!status.isOk()) {
+            switch (status.getFailReason()) {
+                case kvs::Status::FailReason::kNodeNotFound: {
+                    std::cout << "Fail putting value: node not found: " << key << std::endl;
+                    break;
+                }
+                case kvs::Status::FailReason::kKeyNotFound: {
+                    std::cout << "Fail putting value: key not found: " << key << std::endl;
+                    break;
+                }
+                case kvs::Status::FailReason::kOk: {
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < 101; i++) {
+        auto key = "key_" + std::to_string(i);
+        kvs::Value value;
+        auto status = storage->getValue({key, "A/B"}, &value);
+        if (!status.isOk()) {
+            switch (status.getFailReason()) {
+                case kvs::Status::FailReason::kNodeNotFound: {
+                    std::cout << "Fail getting value: node not found: " << key << std::endl;
+                    break;
+                }
+                case kvs::Status::FailReason::kKeyNotFound: {
+                    std::cout << "Fail getting value: key not found: " << key << std::endl;
+                    break;
+                }
+                case kvs::Status::FailReason::kOk: {
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < 101; i++) {
+        auto key = "key_" + std::to_string(i);
+        kvs::Value value;
+        auto status = storage->getValue({key, "A/B/C"}, &value);
+        if (!status.isOk()) {
+            switch (status.getFailReason()) {
+                case kvs::Status::FailReason::kNodeNotFound: {
+                    std::cout << "Fail getting value: node not found: " << key << std::endl;
+                    break;
+                }
+                case kvs::Status::FailReason::kKeyNotFound: {
+                    std::cout << "Fail getting value: key not found: " << key << std::endl;
+                    break;
+                }
+                case kvs::Status::FailReason::kOk: {
+                    break;
+                }
+            }
+        }
+    }
 }
