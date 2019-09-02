9e2a1c6f1afa1498abf34e32d5075d605a39aa54
ddovod <ddovod@gmail.com>
2019-09-02 22:09:41 +0300

    fixing warnings in index-cpp

--- libs/index-cpp/src/linear_hashing_table.h
+++ libs/index-cpp/src/linear_hashing_table.h
@@ -27,10 +27,34 @@
 #include <stdexcept>
 #include <tuple>
 #include <vector>
+#include <functional>
+#include <unordered_set>
 
 // Non-portable Linux-specific headers for file truncation.
 #include <unistd.h>
 #include <sys/types.h>
+
+
+#ifdef _MSC_VER
+#pragma warning ( push )
+#pragma warning(disable:4996)
+#pragma warning(disable:4355)
+#pragma warning(disable:4244)
+#endif
+
+#if defined(__clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wconversion"
+#   pragma clang diagnostic ignored "-Wshorten-64-to-32"
+#   pragma clang diagnostic ignored "-Wsign-compare"
+#   pragma clang diagnostic ignored "-Wunused-parameter"
+#elif defined(__GNUC__)
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wconversion"
+#   pragma GCC diagnostic ignored "-Wsign-compare"
+#   pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
 
 #include "bitarray.h"
 #include "buffer.h"
@@ -678,7 +702,7 @@ public:
     initialize();
   }
 
-  ~LinearHashingTable() {
+  virtual ~LinearHashingTable() {
     bucket_manager_->flush();
   }
 
@@ -890,3 +914,15 @@ private:
     return result;
   }
 };
+
+
+
+#ifdef _MSC_VER
+#pragma warning ( pop )
+#endif
+
+#if defined(__clang__)
+#   pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#   pragma GCC diagnostic pop
+#endif
