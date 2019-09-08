c8d29b6d4b90128b388fc52f54029ff71c607b01
ddovod <ddovod@gmail.com>
2019-09-08 15:31:00 +0300

    rolling back

--- src/IStorage.hpp
+++ src/IStorage.hpp
@@ -13,7 +13,7 @@ namespace kvs
     class IStorage
     {
     public:
-        // virtual ~IStorage() = default;
+        virtual ~IStorage() = default;
 
         virtual Status getValue(const Key& key, Value* value) = 0;
         virtual Status putValue(const Key& key, const Value& value) = 0;
